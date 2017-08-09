2cb6a034a1368caa0019b9c8c1f7507e7d409bad
nick <maildw.01@gmail.com>
2017-08-09 19:11:44 +0300

    Initial commit

--- ps.rb
+++ ps.rb
@@ -1,10 +1,12 @@
 #!/usr/bin/env ruby
 
 #Load the user defined parameters
-
 if (ARGV.length < 1)
 puts "To see every process on the system use -e"
 else
+####
+#we need to add some more keys and process them here
+####
 
 pids = Dir.glob("/proc/[0-9]*")
 
