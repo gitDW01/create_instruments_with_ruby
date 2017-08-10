5045a45b2204bf31b20f193eee26de295f67f83e
nick <maildw.01@gmail.com>
2017-08-10 12:51:49 +0300

    Added -e processing

--- ps.rb
+++ ps.rb
@@ -1,22 +1,22 @@
 #!/usr/bin/env ruby
 
-#Load the user defined parameters
 if (ARGV.length < 1)
-puts "To see every process on the system use -e"
-else
-####
-#we need to add some more keys and process them here
-####
-
-pids = Dir.glob("/proc/[0-9]*")
-
-puts "PID\tCMD"
-puts "-" * 15
-
-pids.each do |pid|
-  cmd = File.read(pid + "/comm")
-  pid = pid.scan(/\d+/).first
-
-  puts "#{pid}\t#{cmd}"
+  puts "To see every process on the system use -e"
+  exit
 end
+
+#Load the user defined parametres
+if ("#{ARGV[0]}" == "-e")
+  pids = Dir.glob("/proc/[0-9]*")
+
+  puts "PID\tCMD"
+  puts "-" * 15
+  
+  pids.each do |pid|
+    cmd = File.read(pid + "/comm")
+    pid = pid.scan(/\d+/).first
+
+    puts "#{pid}\t#{cmd}"
+  end
 end
+
